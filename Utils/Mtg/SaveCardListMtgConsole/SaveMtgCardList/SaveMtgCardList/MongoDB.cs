e1721a95a00c9d2547d4c5002ab05bb38ba59fe5
Simone Zoppelletto <zoppellettosimone@gmail.com>
2022-09-28 17:31:34 +0200

    Update SaveCardList

---
+++ Utils/Mtg/SaveCardListMtgConsole/SaveMtgCardList/SaveMtgCardList/MongoDB.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaveMtgCardList
+{
+    class MongoDB
+    {
+        public static MongoClient Client = new MongoClient("mongodb://127.0.0.1:27017");
+
+        public static bool IsConnected()
+        {
+            try
+            {
+                return MongoDB.Client.GetDatabase("MyTCGCardApp").RunCommandAsync((Command<BsonDocument>)"{ping:1}").Wait(2000);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
--- Utils/Mtg/SaveCardListMtgConsole/SaveMtgCardList/SaveMtgCardList/Program.cs
+++ Utils/Mtg/SaveCardListMtgConsole/SaveMtgCardList/SaveMtgCardList/Program.cs
@@ -1,24 +1,27 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using MongoDB.Driver;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Threading;
 
 namespace SaveMtgCardList
 {
     class Program
     {
-
         //Url API
-        static string apiUrl = "https://api.scryfall.com/cards/search?order=cmc&q=a";
+        static string apiUrl = "https://api.scryfall.com/cards/search?order=cmc&q=e";
         //static string apiUrl = "https://api.scryfall.com/cards/search?order=cmc&q=brainstorm";    //Usata per Test
 
+        //Contatore delle varie pagine dci risposta dell'API
+        static int countPage = 0;
 
-        //Dove verrà salvata la risposta dell'API elaborata
-        static List<Dictionary<string, object>> apiResList = new List<Dictionary<string, object>>();
+        //Numero carte totali, modificato dall'api
+        static int totalCards = -1;
 
         static void Main(string[] args)
         {
@@ -41,9 +44,14 @@ namespace SaveMtgCardList
 
         static void callApi()
         {
+
+            //Aumento il counter delle pagine
+            countPage++;
+
+            Console.WriteLine($"[{DateTime.Now}] -> {countPage}");
+
             //Stringa Json dalla chiamata Api
             string jsonString = "";
-
 
             //Chiamata Api
             Uri address = new Uri(apiUrl);
@@ -63,56 +71,144 @@ namespace SaveMtgCardList
                 //Conversione da Json String a Json
                 var allJsonContent = JObject.Parse(jsonString);
 
+                //Salvo il numero totale di carte
+                if (totalCards == -1)
+                {
+                    totalCards = int.Parse(allJsonContent["total_cards"].ToString());
+
+                    Console.WriteLine($"Numero totale di carte: {totalCards}");
+                }
+
+                //Salva tutti i dati nel db
+                var jsonFromAPI = allJsonContent["data"].Select(s => new Dictionary<string, object> {
+                            { "id", s["id"].ToString() },
+                            { "name", s["name"].ToString() },
+                            { "scryfall_uri", s["scryfall_uri"].ToString()},
+                            { "oracle_text", s["oracle_text"].ToString()},
+                            { "type_line", s["type_line"].ToString()},
+                            { "img", s["image_uris"]["normal"].ToString()},
+                            { "mana_cost", s["mana_cost"].ToString()},
+                            { "cmc", s["cmc"].ToString()},
+                            { "colors", s["colors"].ToString()},
+                            { "color_identity", s["color_identity"].ToString()},
+                            { "reserved", s["reserved"].ToString()},
+                            { "price", s["prices"]["eur"].ToString()},
+                            { "legalities", s["legalities"].Select(p => new Dictionary<string, object> {
+                                            { "standard", p["standard"].ToString() },
+                                            { "future", p["future"].ToString() },
+                                            { "historic", p["historic"].ToString() },
+                                            { "gladiator", p["gladiator"].ToString() },
+                                            { "pioneer", p["pioneer"].ToString() },
+                                            { "explorer", p["explorer"].ToString() },
+                                            { "modern", p["modern"].ToString() },
+                                            { "legacy", p["legacy"].ToString() },
+                                            { "pauper", p["pauper"].ToString() },
+                                            { "vintage", p["vintage"].ToString() },
+                                            { "penny", p["penny"].ToString() },
+                                            { "commander", p["commander"].ToString() },
+                                            { "brawl", p["brawl"].ToString() },
+                                            { "historicbrawl", p["historicbrawl"].ToString() },
+                                            { "alchemy", p["alchemy"].ToString() },
+                                            { "paupercommander", p["paupercommander"].ToString() },
+                                            { "duel", p["duel"].ToString() },
+                                            { "oldschool", p["oldschool"].ToString() },
+                                            { "premodern", p["premodern"].ToString() },
+                                        })
+                            .ToList()} }).ToList();
+
+                //Per ogni elemento preso dall'API (lo salvo su MongoDB)
+                foreach (var item in jsonFromAPI)
+                {
+                    try
+                    {
+                        //Controllo connessione MongoDB
+                        if (MongoDB.IsConnected())
+                        {
+                            //Prende la lista delle Collection e controlla se esiste la collection MtgCards
+                            //DA TESTARE
+                            var listCollection = MongoDB.Client.GetDatabase("a").ListCollectionNames();
+                            //SE NON TROVA NESSUNA CORRISPONDENZA NEL NOME
+                            if (listCollection.Where(s => s).Count() == 0)
+                            {
+                                //CREA LA COLLECTION
+                            }
+
+                            //Prendo tutti gli elementi esistenti nella collection
+                            var mongoDbCardList = MongoDB.Client
+                    .GetDatabase("a")
+                    .GetCollection<Dictionary<string, object>>("a")
+                   .Find(Builders<Dictionary<string, object>>.Filter.Empty)
+                   .ToList();
+
+                            //Controllo se esiste già e se non esiste la inserisco
+                            if (mongoDbCardList.Where(s => s["name"].ToString().ToLower().Equals(item["name"].ToString().ToLower())).Count() == 0)
+                            {
+                                //Nuovo Dict da inserire
+                                Dictionary<string, object> dictToInsert = new Dictionary<string, object>();
+                                dictToInsert.Add("id", item["id"].ToString());
+                                dictToInsert.Add("name", item["name"].ToString());
+                                dictToInsert.Add("scryfall_uri", item["scryfall_uri"].ToString());
+                                dictToInsert.Add("oracle_text", item["oracle_text"].ToString());
+                                dictToInsert.Add("type_line", item["type_line"].ToString());
+                                dictToInsert.Add("img", item["img"].ToString());
+                                dictToInsert.Add("mana_cost", item["mana_cost"].ToString());
+                                dictToInsert.Add("cmc", item["cmc"].ToString());
+                                dictToInsert.Add("colors", item["colors"].ToString());
+                                dictToInsert.Add("color_identity", item["color_identity"].ToString());
+                                dictToInsert.Add("reserved", item["reserved"].ToString());
+                                dictToInsert.Add("price", item["price"].ToString());
+                                dictToInsert.Add("legalities", item["legalities"].ToString());
+
+                                //InsertOne MongoDb
+                                MongoDB.Client.GetDatabase("a")
+                                    .GetCollection<Dictionary<string, object>>("a")
+                                    .InsertOne(dictToInsert);
+                            }
+
+                            /*
+                            //NEL CASO DI UPDATE, TENGO PER IL FUTURO TEST
+                            if (action == EAction.Update)
+                            {
+                                //Filtro di ricerca
+                                var searchFilter = Builders<Dictionary<string, object>>.Filter.Eq("name", cardNameOld)
+                                     & Builders<Dictionary<string, object>>.Filter.Eq("nrCopies", nrCopiesOld);
+
+                                //Update MongoDb
+                                MongoDB.Client.GetDatabase("yugiohCardDb").GetCollection<Dictionary<string, object>>(nameCollection + "Wishlist")
+                                        .UpdateOne(searchFilter, Builders<Dictionary<string, object>>
+                                        .Update.Set("name", cardName).Set("nrCopies", nrCopies));
+
+                            }
+                            */
+                        }
+                    }
+                    catch (Exception err)
+                    {
+                        Console.WriteLine($"{item["name"]}\n{err.StackTrace}\n{err.Message}");
+                    }
+                }
+
                 if (allJsonContent.ContainsKey("next_page"))
                 {
-                    Console.WriteLine($"[{DateTime.Now}] Yes");
-
                     //Nuova API da chiamare
                     apiUrl = allJsonContent["next_page"].ToString();
 
-                    //Tempo di attesa per l'API
-                    Thread.Sleep(200);
+                    //Tempo di attesa per l'API (Richiesti 100 ms tra le varie chiamate, considerando il resto del codice potrebbe anche non servire)
+                    Thread.Sleep(100);
 
                     //RIchiama l'API
                     callApi();
                 }
                 else
                 {
+                    //Fine inserimento carte
+                    Console.WriteLine($"[{DateTime.Now}] -> END (Lista creata/modificata correttamente)");
 
-                    Console.WriteLine($"[{DateTime.Now}] Nope (Lista creata correttamente)");
-
-                    // Per Test
-                    var test = allJsonContent["data"].Where(p => p["name"].ToString().ToLower().Contains("brainstorm")).ToList();
                 }
-
-                /*
-
-                //Estrazione dei dati
-                apiResList = allJsonContent["data"].Select(s => new Dictionary<string, object> {
-                            { "name", s["name"].ToString() },
-                            { "type", s["type"].ToString()},
-                            { "race", s["race"].ToString()},
-                            { "desc", s["desc"].ToString()},
-                            { "attribute", s.ToObject<JObject>().ContainsKey("attribute") == true && s["atk"].ToString() != "" ? s["attribute"].ToString() : "Null" },
-                            { "atk", s.ToObject<JObject>().ContainsKey("atk") == true && s["atk"].ToString() != "" ? int.Parse(s["atk"].ToString()) : 0},
-                            { "def", s.ToObject<JObject>().ContainsKey("def") == true && s["def"].ToString() != "" ? int.Parse(s["def"].ToString()) : 0},
-                            { "level", s.ToObject<JObject>().ContainsKey("level") == true && s["level"].ToString() != "" ? int.Parse(s["level"].ToString()) : 0},
-                            { "linkval", s.ToObject<JObject>().ContainsKey("linkval") == true && s["linkval"].ToString() != "" ? int.Parse(s["linkval"].ToString()) : 0},
-                            { "imageId", s["card_images"][0]["id"].ToString()},
-                            { "price", s["card_prices"][0]["cardmarket_price"].ToString()},
-                            { "sets", s.ToObject<JObject>().ContainsKey("card_sets") == true ? s["card_sets"].Select(p => new Dictionary<string, object> {
-                                { "set", p["set_code"].ToString() }
-                            }).ToList() : new List<Dictionary<string, object>>()}
-                                })
-                                .ToList();
-
-                */
-
-
-
             }
             catch (Exception err)
             {
+                //In caso di errore con API o altro
                 string Message = $"{err.StackTrace}\n{err.Message}";
                 Console.WriteLine(Message);
             }
